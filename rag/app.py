aa5eccbffc0ba6af0ac17c5bcf4188cf020f7a84
Cole-nnn <laicola930527@gmail.com>
2025-01-24 10:49:51 +0800

    Update app.py

--- rag/app.py
+++ rag/app.py
@@ -4,35 +4,34 @@ from langchain_community.embeddings import OpenAIEmbeddings
 from langchain_community.vectorstores import Chroma
 from langchain.chains.question_answering import load_qa_chain
 from langchain_community.callbacks import get_openai_callback
-from langchain_community.chat_models import ChatOpenAI  # 更新的導入語句
+from langchain_community.chat_models import ChatOpenAI
 from opencc import OpenCC
 import openai
 
-# 使用環境變數來存儲 OpenAI API 金鑰
+# 設定 OpenAI API 金鑰
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='rag/templates', static_folder='rag/static')
 
-# 假設這是你的聊天歷史記錄
+# 聊天歷史記錄
 chat_history = []
 
-# 設置首頁路由，返回一個 HTML 頁面
 @app.route('/')
 def home():
-    return render_template('index.html')  # 確保你的模板文件夾下有 index.html
+    return render_template('index.html')
 
 @app.route("/get_response", methods=["POST"])
 def get_response():
-    user_input = request.form.get('user_input')
+    user_input = request.json.get('question')
     if not user_input:
         return jsonify({'error': 'No user input provided'})
 
     embeddings = OpenAIEmbeddings()
-    db = Chroma(persist_directory="./db/temp/", embedding_function=embeddings)
+    db = Chroma(persist_directory="./rag/db/temp/", embedding_function=embeddings)
     docs = db.similarity_search(user_input)
     
     llm = ChatOpenAI(
-        model_name="gpt-4",  # 確保使用的是正確的模型名稱
+        model_name="gpt-4",
         temperature=0.5
     )
     chain = load_qa_chain(llm, chain_type="stuff")
@@ -43,7 +42,6 @@ def get_response():
     cc = OpenCC('s2t')
     answer = cc.convert(response['output_text'])
 
-    # 這裡可以將聊天歷史記錄儲存到 session 或其他地方
     chat_history.append({'user': user_input, 'assistant': answer})
 
     return jsonify({'response': answer})
